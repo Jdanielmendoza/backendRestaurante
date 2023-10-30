a70fc2bbcd556b0e383d55bdfa14a094016fbb02
Jose Daniel Mendoza <jdanielmendoza987@gmail.com>
2023-10-30 08:48:41 -0400

    se agrego consulta para obtener categorias

--- src/components/categoria/categoria.model.js
+++ src/components/categoria/categoria.model.js
@@ -1,52 +1,57 @@
 import { pool } from "../../config/databaseConnection.js";
 
-export const crearCategoria=async(id,nombre,descripcion,id_categoria)=>{
-  try {
-    const client= await pool.connect();
-    const res= await pool.query("INSERT INTO CATEGORIA(id,nombre,descripcion,id_categoria) values($1,$2,$3,$4)",[id,nombre,descripcion,id_categoria]);
-    client.release();
-    return res
-  } catch (error) {
-    client.release();
-    return error
-  }
-}
-
-export const obtenerCategoria = async () => {
+export const crearCategoria = async (id, nombre, descripcion, id_categoria) => {
   try {
     const client = await pool.connect();
-    const res = await pool.query("SELECT * FROM CATEGORIA")
-    client.release();
-    return res.rows;
-  } catch (error) {
-   client.release();
-   return error
-  }
-
-}
-
-export const eliminarCategoria=async(id)=>{
-  try {
-    const client=await pool.connect();
-    const res=await pool.query("DELETE FROM CATEGORIA WHERE id=$1",[id]);
+    const res = await pool.query(
+      "INSERT INTO CATEGORIA(id,nombre,descripcion,id_categoria) values($1,$2,$3,$4)",
+      [id, nombre, descripcion, id_categoria]
+    );
     client.release();
     return res;
   } catch (error) {
     client.release();
-    return error
+    return error;
   }
-}
+};
 
-export const actualizarCategoria=async(id,nombre,descripcion)=>{
+export const obtenerCategoria = async () => {
   try {
-    const client=await pool.connect();
-    const res= await pool.query("UPDATE CATEGORIA SET nombre=$1,descripcion=$2  WHERE id=$3",[nombre,descripcion,id]);
+    const client = await pool.connect();
+    const res = await pool.query(
+      "SELECT CATEGORIA_HIJO.*, CATEGORIA_PADRE.nombre AS categoria_padre FROM CATEGORIA AS CATEGORIA_HIJO LEFT JOIN CATEGORIA AS CATEGORIA_PADRE ON CATEGORIA_HIJO.id_categoria =   CATEGORIA_PADRE.id;"
+    );
     client.release();
-    return res
-    
+    return res.rows;
   } catch (error) {
     client.release();
-    return error
+    return error;
   }
-}
+};
 
+export const eliminarCategoria = async (id) => {
+  try {
+    const client = await pool.connect();
+    const res = await pool.query("DELETE FROM CATEGORIA WHERE id=$1", [id]);
+    client.release();
+    return res;
+  } catch (error) {
+    client.release();
+    return error;
+  }
+};
+
+export const actualizarCategoria = async (id, nombre, descripcion) => {
+  try {
+    const client = await pool.connect();
+    const res = await pool.query(
+      "UPDATE CATEGORIA SET nombre=$1,descripcion=$2  WHERE id=$3",
+      [nombre, descripcion, id]
+    );
+    client.release();
+    return res;
+  } catch (error) {
+    client.release();
+    return error;
+  }
+};
