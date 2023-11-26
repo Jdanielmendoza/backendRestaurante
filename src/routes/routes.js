743448c253fd2e0d99ff2784ebaddc9671e26aa1
Micael Cardona <micaelcardona@hotmail.com>
2023-11-26 10:59:40 -0400

    añadido crud de insumos

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -5,6 +5,7 @@ import routerTable from "../components/mesa/mesa.routes.js"
 import routerCategory from "../components/categoria/categoria.routes.js";import routerProducto from "../components/producto/producto.routes.js";
 import pagoRouter from "../components/pago/pago.routes.js";
 import pedidoRouter from "../components/pedido/pedido.routes.js";
+import insumoRouter from "../components/insumos/insumos.routes.js";
 
 const router = Router(); 
 router.use('/mesa',routerTable);
@@ -13,7 +14,8 @@ router.use('/auth', authRouter);
 router.use('/categoria',routerCategory);
 router.use('/producto', routerProducto);
 router.use('/tipoDePago',pagoRouter);
-router.use('/pedido',pedidoRouter);
+router.use('/pedido',pedidoRouter); 
+router.use('/insumo',insumoRouter);
 
 export default router ;
 
